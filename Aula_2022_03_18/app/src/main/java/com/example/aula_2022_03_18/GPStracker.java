0bd92c88297f0e3e89e2f03b92e3688ebfc9fcfd
Aluno <lucas.nazato@hotmail.com>
2022-03-18 22:13:40 -0300

    Comentários adicionados

--- Aula_2022_03_18/app/src/main/java/com/example/aula_2022_03_18/GPStracker.java
+++ Aula_2022_03_18/app/src/main/java/com/example/aula_2022_03_18/GPStracker.java
@@ -15,42 +15,47 @@ import androidx.core.content.ContextCompat;
 public class GPStracker implements LocationListener {
 
     Context context;
-    //
+
+    // Criar metodo publico do GPStracker
     public GPStracker(Context c) {
         context = c;
     }
 
-    //
+    // Metodo publico derivado do LocationListener
     public Location getLocation(){
-//
+
+        // Checar a permissão para usar a localização no Manifest, caso sim, pegar se o GPS está ativado
         if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) !=
                 PackageManager.PERMISSION_GRANTED){
-//
-            Toast.makeText(context, "Não foi permitir", Toast.LENGTH_SHORT).show();
+
+            // Se não for permitido pelo Package Manager, mostrar mensagem ao usuário avisando e retornar null
+            Toast.makeText(context, "Não foi permitido", Toast.LENGTH_SHORT).show();
             return null;
         }
         LocationManager lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         boolean isGPSEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-        //
+        // Se o GPS estiver ativado, pegar a localização e a retornar
         if (isGPSEnabled){
             lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 10, this);
             Location l = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             return l;
         } else {
-//
-            Toast.makeText(context, "Por favor, habitar o GPS!", Toast.LENGTH_LONG).show();
+
+            // Caso não esteja ativado, pedir ao usuário que ative o GPS
+            Toast.makeText(context, "Por favor, habilitar o GPS!", Toast.LENGTH_LONG).show();
         }
-//
+
+        // Após pegar todas as informações retornar nulo
         return null;
     }
-    //
+    // Precisa sobrescrever para que o metódo funcione
     @Override
     public void onProviderDisabled(@NonNull String provider) {    }
-    //
+    // Precisa sobrescrever para que o metódo funcione
     @Override
     public void onLocationChanged(@NonNull Location location) {    }
-    //
+    // Precisa sobrescrever para que o metódo funcione
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {    }
 }
--- Aula_2022_03_18/app/src/main/java/com/example/aula_2022_03_18/MainActivity.java
+++ Aula_2022_03_18/app/src/main/java/com/example/aula_2022_03_18/MainActivity.java
@@ -22,24 +22,24 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageViewFoto;
     private Button btnGeo;
 
-    // 
+    // Sobrescrever o OnCreate para criar a visualização do app
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Atribuir botao
+        // Atribuir botao do xml a variavel e pegar permissão de localização
         btnGeo = (Button) findViewById(R.id.btn_gps);
         ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
 
-        //
+        // Pegar o botão e ao clicar realizar a ação
         btnGeo.setOnClickListener(new View.OnClickListener() {
-            //
+            // Sobrescrever o OnClick e criar a classe GPStracker e pegar a localização
             @Override
             public void onClick(View view) {
                 GPStracker g = new GPStracker(getApplication());
                 Location l = g.getLocation();
-                // Se localização não for nula, pegar a latitude e longitude, concatenar e escrever na tela
+                // Se localização não for nula, pegar a latitude e longitude da localização, concatenar e escrever na tela
                 if(l != null) {
                     double lat = l.getLatitude();
                     double lon = l.getLongitude();
@@ -53,11 +53,11 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 0);
         }
 
-        // Declarar
+        // Declarar a variavel de imagem e atribuir a imagem no xml
         imageViewFoto = (ImageView) findViewById(R.id.image_foto);
         findViewById(R.id.btn_pic).setOnClickListener(new View.OnClickListener() {
 
-            // Sobrescrever OnClickView
+            // Sobrescrever OnClickView para chamar o metodo de tirar a foto
             @Override
             public void onClick(View view) {
                 tirarFoto();
@@ -65,16 +65,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // Usar permissão da camera para capturar a foto e carregar
     private void tirarFoto() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, 1);
     }
 
-    //
+    // Sobrescrever o onActivityResult e passar os dados do intent
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
-        //
+        // Se for código certo e o resultado for OK, usar o bundle para pegar a imagem que foi capturada e passar para a var ImageViewFoto
         if(requestCode == 1 && resultCode == RESULT_OK){
             Bundle extras = data.getExtras();
             Bitmap imagem = (Bitmap) extras.get("data");
