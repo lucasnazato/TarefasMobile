5f60cc6bf9eb6149e6565fa1144a3c547d6a201e
Aluno <lucas.nazato@hotmail.com>
2022-03-18 21:14:51 -0300

    Main e GPStracker adicionados

--- Aula_2022_03_18/app/src/main/java/com/example/aula_2022_03_18/MainActivity.java
+++ Aula_2022_03_18/app/src/main/java/com/example/aula_2022_03_18/MainActivity.java
@@ -1,14 +1,85 @@
 package com.example.aula_2022_03_18;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.Location;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Criar variaveis para imagem e botão
+    private ImageView imageViewFoto;
+    private Button btnGeo;
+
+    // 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Atribuir botao
+        btnGeo = (Button) findViewById(R.id.btn_gps);
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+
+        //
+        btnGeo.setOnClickListener(new View.OnClickListener() {
+            //
+            @Override
+            public void onClick(View view) {
+                GPStracker g = new GPStracker(getApplication());
+                Location l = g.getLocation();
+                // Se localização não for nula, pegar a latitude e longitude, concatenar e escrever na tela
+                if(l != null) {
+                    double lat = l.getLatitude();
+                    double lon = l.getLongitude();
+                    Toast.makeText(getApplicationContext(), "LATITUDE: " + lat +"\n LONGITUDE: " + lon, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        // Checar permissão de uso da camera no Manifest
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 0);
+        }
+
+        // Declarar
+        imageViewFoto = (ImageView) findViewById(R.id.image_foto);
+        findViewById(R.id.btn_pic).setOnClickListener(new View.OnClickListener() {
+
+            // Sobrescrever OnClickView
+            @Override
+            public void onClick(View view) {
+                tirarFoto();
+            }
+        });
+    }
+
+    private void tirarFoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+
+    //
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        //
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imagem = (Bitmap) extras.get("data");
+            imageViewFoto.setImageBitmap(imagem);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
