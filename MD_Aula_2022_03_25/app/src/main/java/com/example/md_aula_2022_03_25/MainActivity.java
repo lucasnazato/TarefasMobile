8d6823b19a27b167d011f96de47f1665c2da3dbb
Aluno <lucas.nazato@hotmail.com>
2022-03-25 20:14:29 -0300

    Tarefa Mobile 25/03/2022

---
+++ MD_Aula_2022_03_25/app/src/main/java/com/example/md_aula_2022_03_25/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.md_aula_2022_03_25;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_personagem);
+    }
+}
